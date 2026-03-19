b2c57056e6cd419f240500fb4ba3d02506a5ea08
Yeferson Sossa <yeferson1115@gmail.com>
2026-03-18 21:17:43 -0500

    Fix null filter handling for ZIP export job

---
+++ evaluador-api/tests/Unit/GenerateCertificadosZipJobTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Jobs\GenerateCertificadosZipJob;
+use PHPUnit\Framework\TestCase;
+
+class GenerateCertificadosZipJobTest extends TestCase
+{
+    public function test_it_accepts_a_null_filter_when_dispatching_the_job(): void
+    {
+        $job = new GenerateCertificadosZipJob(123, null, [1, 2, 3], true);
+
+        $this->assertInstanceOf(GenerateCertificadosZipJob::class, $job);
+    }
+}
